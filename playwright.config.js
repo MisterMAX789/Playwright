a268cfa1409462d35c7198b524075a7b7ab2ca65
MisterMAX789 <maximotruc@mail.ru>
2023-01-14 01:05:09 +0200

    change gitignore

---
+++ playwright.config.js
@@ -0,0 +1,10 @@
+const config = {
+    use: {
+        headless: false,
+        viewport: { width: 1280, height: 720 },
+        slowMo: 5000,
+        ignoreHTTPSErrors: true,
+    },
+};
+
+module.exports = config;
